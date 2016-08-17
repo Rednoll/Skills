6cc59e71b96501522d523c6c3f009c4f61d29ec5
gt22 <igorengel@mail.ru>
2016-08-17 05:33:49 +0300

    Бейте меня по голове

--- src/main/java/inwaiders/redn/rpg/base/core/Core.java
+++ src/main/java/inwaiders/redn/rpg/base/core/Core.java
@@ -30,7 +30,7 @@ public class Core
 	public static final String MODID = "rpg";
 	public static final String VERSION = "1.0";
 	public static final String NAME = "RPG";
-	public static final String path = "inwaiders.redn.rpg.base";
+	public static final String path = "inwaiders.redn.rpg.base.proxy";
 	public static final ResourceLocGenerator skillrlgen = new ResourceLocGenerator(MODID, "textures/sicons/", ".png");
 	public static Random r = new Random();
 
