6e4cd448b719b7d19d4bc81b411b55ebd4b70761
gt22 <igorengel@mail.ru>
2016-08-18 15:06:45 +0300

    Fixed registry

--- src/main/java/inwaiders/redn/rpg/registry/ItemSkillRegistry.java
+++ src/main/java/inwaiders/redn/rpg/registry/ItemSkillRegistry.java
@@ -9,28 +9,26 @@ import cpw.mods.fml.common.FMLLog;
 
 public class ItemSkillRegistry {
 	private static HashMap<Integer, Class<? extends ItemSkillBase>> skills = new HashMap<Integer, Class<? extends ItemSkillBase>>();
-	
-	public static void register(int id, Class<? extends ItemSkillBase> skill)
-	{
-		if(skills.containsKey(id))
-		{
+
+	public static void register(int id, Class<? extends ItemSkillBase> skill) {
+		if (skills.containsKey(id)) {
 			FMLLog.warning("ItemSpell with id " + id + " alredy registered, skipping");
 			return;
 		}
 		skills.put(id, skill);
 	}
-	
-	public static void register(ItemSkillBase skill)
-	{
+
+	public static void register(ItemSkillBase skill) {
 		register(skill.getId(), skill.getClass());
 	}
-	
-	public static ItemSkillBase getById(int id)
-	{
-		try {
-			return skills.get(id).newInstance();
-		} catch (Exception e) {
-			MiscUtils.crashGame("Unable to get ItemSkill with id " + id + " from registry, contact mod author", e);
+
+	public static ItemSkillBase getById(int id) {
+		if (skills.containsKey(id)) {
+			try {
+				return skills.get(id).newInstance();
+			} catch (Exception e) {
+				MiscUtils.crashGame("Unable to get ItemSkill with id " + id + " from registry, contact mod author", e);
+			}
 		}
 		return null;
 	}
