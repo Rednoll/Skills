d176d1db6d97a1cbc8ccb58a0dfc8cc7895feee6
gt22 <igorengel@mail.ru>
2016-08-18 14:55:02 +0300

    Removed cast type form ItemSkillBase

--- src/main/java/inwaiders/redn/rpg/skills/armor/ItemSkillBase.java
+++ src/main/java/inwaiders/redn/rpg/skills/armor/ItemSkillBase.java
@@ -11,16 +11,10 @@ public abstract class ItemSkillBase {
 		TICK, HITWEARER, /* Armor */
 		HITTARGET, /* Sword */
 	}
-
-	protected int coolDown = 0;
 	protected int level = 1;
 	protected ItemSkillType type;
 	protected int[] damage;
 	protected int[] radius;
-	protected int[] maxCast;
-	protected int[] maxCoolDown;
-	protected int cast = 0;
-	protected boolean casting = false;
 	protected int interval = 0;
 	protected int maxInterval = 0;
 	protected Target target;
@@ -31,8 +25,6 @@ public abstract class ItemSkillBase {
 		MAX_SKILL_LVL = maxLvl;
 		damage = new int[MAX_SKILL_LVL];
 		radius = new int[MAX_SKILL_LVL];
-		maxCast = new int[MAX_SKILL_LVL];
-		maxCoolDown = new int[MAX_SKILL_LVL];
 	}
 	
 	public ItemSkillBase() {
@@ -47,20 +39,6 @@ public abstract class ItemSkillBase {
 	public int getRadius() {
 		int lvl = getLevel();
 		return radius[lvl] == 0 ? radius[1] * lvl : radius[lvl];
-	}
-
-	public int getMaxCoolDown() {
-		int lvl = getLevel();
-		return maxCoolDown[lvl] == 0 ? maxCoolDown[1] * lvl : maxCoolDown[lvl];
-	}
-
-	public int getMaxCast() {
-		int lvl = getLevel();
-		return maxCast[lvl] == 0 ? getMaxCast(lvl - 1) : maxCast[lvl];
-	}
-
-	private int getMaxCast(int lvl) {
-		return maxCast[lvl] == 0 ? getMaxCast(lvl - 1) : maxCast[lvl];
 	}
 
 	public int getMaxInterval() {
@@ -79,24 +57,12 @@ public abstract class ItemSkillBase {
 		return interval;
 	}
 
-	public int getCast() {
-		return cast;
-	}
-
 	public Target getTarget() {
 		return target;
 	}
 
 	public ItemSkillType getType() {
 		return type;
-	}
-
-	public int getCoolDown() {
-		return coolDown;
-	}
-
-	public void skillStart(EntityPlayer ep) {
-
 	}
 
 	public void preWhileUpdate(EntityPlayer ep) {
@@ -112,36 +78,6 @@ public abstract class ItemSkillBase {
 
 	public void whileUpdate(EntityPlayer ep) {
 
-	}
-
-	public void preCast(EntityPlayer ep) {
-
-		if (casting) {
-
-			if (getCast() >= getMaxCast()) {
-
-				cast = 0;
-				casting = false;
-				if (!ep.capabilities.isCreativeMode)
-					coolDown = getMaxCoolDown();
-				skillStart(ep);
-			}
-
-			castUpdate(ep);
-
-			cast++;
-		}
-
-	}
-
-	public void castUpdate(EntityPlayer ep) {
-
-	}
-
-	public void startCasting() {
-		if (coolDown <= 0) {
-			casting = true;
-		}
 	}
 
 	public abstract int getId();
