2de76dcefde3aa2935bdfaf4b29488ea718b05e7
gt22 <igorengel@mail.ru>
2016-08-19 15:11:28 +0300

    Mmm

---
+++ src/main/java/inwaiders/redn/rpg/items/itemskills/IS.java
@@ -0,0 +1,11 @@
+package inwaiders.redn.rpg.items.itemskills;
+
+import inwaiders.redn.rpg.files.CFG;
+
+public class IS extends ItemSkillItemBase {
+
+	public IS() {
+		super("IS", CFG.InstantShieldID, 1);
+	}
+
+}
