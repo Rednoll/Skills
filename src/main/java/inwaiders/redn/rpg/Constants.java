e0d193c27701915a478e43c8091ff0966bd58c62
stepa glumov <stepa glumov>
2016-08-19 15:45:10 +0300

    fixer

--- src/main/java/inwaiders/redn/rpg/Constants.java
+++ src/main/java/inwaiders/redn/rpg/Constants.java
@@ -1,6 +1,7 @@
 package inwaiders.redn.rpg;
 
 public class Constants {
-	public static final int DEFAUL_NEXT_XP = 1000;
+	public static final int[] DEFAUL_NEXT_XP = new int[]{150, 250, 375, 525, 650};
+	
 	public static final int DEFAUL_MAX_SKILL_LVL = 15;
 }
