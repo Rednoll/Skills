6e116c75b01acbe15ba24c4160fb0c570885166c
gt22 <igorengel@mail.ru>
2016-08-21 23:28:10 +0300

    Fixed adding incredable amount of entries into bank

--- src/main/java/inwaiders/redn/rpg/files/nbt/PlayerNbt.java
+++ src/main/java/inwaiders/redn/rpg/files/nbt/PlayerNbt.java
@@ -9,12 +9,13 @@ public class PlayerNbt {
 	// Names
 	public static final String TAGNAME = "SkillEngine";
 	// Main
-	public static final String HOTBAR = "hotbar";// Object of 6 strings, named by their position
+	public static final String HOTBAR = "hotbar";// Object of 6 strings, named
+													// by their position
 	public static final String BANK = "bank";// Array
 	public static final String TEAM = "team";// String
 	public static final String LEARNPOINTS = "learnpts";// Int
 	public static final String XP = "XP";// Int
-	//BankSkill|Main
+	// BankSkill|Main
 	public static final String LEVEL = "lvl";
 	// Bank skill
 	public static final String NAME = "name";// Int
@@ -22,6 +23,7 @@ public class PlayerNbt {
 
 	public NBTTagCompound nbt;
 	protected EntityPlayer ep;
+
 	public PlayerNbt(EntityPlayer e) {
 		ep = e;
 		if (!e.getEntityData().hasKey(TAGNAME)) {
@@ -33,19 +35,16 @@ public class PlayerNbt {
 		initNbt();
 	}
 
-	public void resetNBT()
-	{
+	public void resetNBT() {
 		initNbt();
 		ep.getEntityData().setTag(TAGNAME, nbt);
 	}
-	
+
 	private void initNbt() {
-		if(!nbt.hasKey(BANK))
-		{
+		if (!nbt.hasKey(BANK)) {
 			initBank();
 		}
-		if(!nbt.hasKey(HOTBAR))
-		{
+		if (!nbt.hasKey(HOTBAR)) {
 			initHotbar();
 		}
 		initMain();
@@ -53,124 +52,114 @@ public class PlayerNbt {
 
 	private void initHotbar() {
 		NBTTagCompound hotbar = new NBTTagCompound();
-		for(int i = 0; i < 6; i++)
-		{
+		for (int i = 0; i < 6; i++) {
 			hotbar.setString(i + "", "NONE");
 		}
 		nbt.setTag(HOTBAR, hotbar);
 	}
-	
+
 	private void initBank() {
 		nbt.setTag(BANK, new NBTTagList());
 	}
-	
+
 	private void initMain() {
-		if(!nbt.hasKey(TEAM))
-		{
+		if (!nbt.hasKey(TEAM)) {
 			nbt.setString(TEAM, "ANY");
 		}
-		if(!nbt.hasKey(LEVEL))
-		{
+		if (!nbt.hasKey(LEVEL)) {
 			nbt.setInteger(LEVEL, 0);
 		}
-		if(!nbt.hasKey(XP))
-		{
+		if (!nbt.hasKey(XP)) {
 			nbt.setInteger(XP, 0);
 		}
-		if(!nbt.hasKey(LEARNPOINTS))
-		{
+		if (!nbt.hasKey(LEARNPOINTS)) {
 			nbt.setInteger(LEARNPOINTS, 0);
 		}
 	}
-	
-	public void addBank(String name, int lvl, int cd)
-	{
-		NBTTagCompound skill = new NBTTagCompound();
-		skill.setString(NAME, name);
-		skill.setInteger(LEVEL, lvl);
-		skill.setInteger(COOLDOWN, cd);
-		nbt.getTagList(BANK, 10).appendTag(skill);
+
+	public void addBank(String name, int lvl, int cd) {
+		if (checkBank(name)) {
+			NBTTagCompound skill = new NBTTagCompound();
+			skill.setString(NAME, name);
+			skill.setInteger(LEVEL, lvl);
+			skill.setInteger(COOLDOWN, cd);
+			nbt.getTagList(BANK, 10).appendTag(skill);
+		}
 	}
-	
-	public BankSkill getBank(int slot)
-	{
+
+	private boolean checkBank(String name) {
+		NBTTagList bank = nbt.getTagList(BANK, 10);
+		for (int i = 0; i < bank.tagCount(); i++) {
+			if (bank.getCompoundTagAt(i).getString(NAME).equals(name)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public BankSkill getBank(int slot) {
 		NBTTagCompound skill = nbt.getTagList(BANK, 10).getCompoundTagAt(slot);
 		return skill.hasKey(NAME) ? new BankSkill(skill.getString(NAME), skill.getInteger(LEVEL), skill.getInteger(COOLDOWN)) : null;
 	}
-	
-	public int getBankSize()
-	{
+
+	public int getBankSize() {
 		return nbt.getTagList(BANK, 10).tagCount();
 	}
-	
-	public void setHotbar(int slot, String name)
-	{
+
+	public void setHotbar(int slot, String name) {
 		nbt.getCompoundTag(HOTBAR).setString(slot + "", name);
 	}
-	
-	public String getHotbar(int slot)
-	{
+
+	public String getHotbar(int slot) {
 		return nbt.getCompoundTag(HOTBAR).getString(slot + "");
 	}
-	
-	public String[] getHotbarSkills()
-	{
+
+	public String[] getHotbarSkills() {
 		NBTTagCompound hotbar = nbt.getCompoundTag(HOTBAR);
 		String[] ret = new String[6];
-		for(int i = 0; i < 6; i++)
-		{
+		for (int i = 0; i < 6; i++) {
 			ret[i] = hotbar.getString(i + "");
 		}
 		return ret;
 	}
-	
-	public void setHotbarSkills(String[] skills)
-	{
+
+	public void setHotbarSkills(String[] skills) {
 		NBTTagCompound hotbar = nbt.getCompoundTag(HOTBAR);
-		for(int i = 0; i < 6; i++)
-		{
+		for (int i = 0; i < 6; i++) {
 			hotbar.setString(i + "", skills[i]);
 		}
 	}
-	
-	public void setTeam(String team)
-	{
+
+	public void setTeam(String team) {
 		nbt.setString(TEAM, team);
 	}
-	
-	public String getTeam()
-	{
+
+	public String getTeam() {
 		return nbt.getString(TEAM);
 	}
-	
-	public void setLearnPoints(int pts)
-	{
+
+	public void setLearnPoints(int pts) {
 		nbt.setInteger(LEARNPOINTS, pts);
 	}
-	
-	public int getLearnPoints()
-	{
+
+	public int getLearnPoints() {
 		return nbt.getInteger(LEARNPOINTS);
 	}
-	
-	public void setXp(int xp)
-	{
+
+	public void setXp(int xp) {
 		nbt.setInteger(XP, xp);
 	}
-	
-	public int getXp()
-	{
+
+	public int getXp() {
 		return nbt.getInteger(XP);
 	}
-	
-	public void setLevel(int lvl)
-	{
+
+	public void setLevel(int lvl) {
 		nbt.setInteger(LEVEL, lvl);
 	}
-	
-	public int getLevel()
-	{
+
+	public int getLevel() {
 		return nbt.getInteger(LEVEL);
 	}
-	
+
 }
