09d10a36866cc335e1b2d86e3fcdd61ec6a3d6c3
gt22 <igorengel@mail.ru>
2016-08-23 11:00:19 +0300

    More default for next xp

--- src/main/java/inwaiders/redn/rpg/Constants.java
+++ src/main/java/inwaiders/redn/rpg/Constants.java
@@ -1,7 +1,7 @@
 package inwaiders.redn.rpg;
 
 public class Constants {
-	public static final int[] DEFAUL_NEXT_XP = new int[]{150, 250, 375, 525, 650};
+	public static final int[] DEFAUL_NEXT_XP = new int[]{150, 250, 375, 525, 650, 700, 725, 775, 850, 1000, 1100, 1170, 1230, 1300};
 	
 	public static final int DEFAUL_MAX_SKILL_LVL = 15;
 }
